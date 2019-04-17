d064529666b7d122db21dcd4861e8b5e6d0f0ab6
SanthaManohar <49111237+SanthaManohar@users.noreply.github.com>
2019-04-17 12:39:49 +0530

    Draw Direction between two points in Google Map

---
+++ LegsObject.java
@@ -0,0 +1,13 @@
+package com.app.directionpointstwo;
+
+import java.util.List;
+
+public class LegsObject {
+    private List<StepsObject> steps;
+    public LegsObject(List<StepsObject> steps) {
+        this.steps = steps;
+    }
+    public List<StepsObject> getSteps() {
+        return steps;
+    }
+}
